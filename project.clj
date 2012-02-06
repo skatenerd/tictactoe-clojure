6b3b843f85380454ca58f145f61ed5a6ae4bb7f0
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-02-06 15:01:18 -0600

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject tictactoe "1.0.0-SNAPSHOT"
+  :description "Immutable Tic Tac Toe"
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[speclj "2.1.1"]]
+  :test-path "spec/"
+)
