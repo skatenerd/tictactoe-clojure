d6f3d7409ccab8dd001bafd254c61878d6c944e4
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-02-08 22:19:27 -0600

    implement minimax scoring

--- spec/tictactoe/smart_ai_player_spec.clj
+++ spec/tictactoe/smart_ai_player_spec.clj
@@ -5,6 +5,13 @@
 
 (describe "basic intelligence"
   (with smart-ai-player (new-smart-ai-player :x))
+  (with x-won-row [[:x :x :x]
+                   [:o :o nil]
+                   [:x :o nil]])
+  (with x-won-col [[:x   nil :o]
+                   [:x   :o  nil]
+                   [:x nil nil]])
+
   (with x-can-win-row [[:x :x nil]
                        [:o :o nil]
                        [:x :o nil]])
@@ -12,8 +19,30 @@
                        [:x   :o  nil]
                        [nil nil nil]])
 
+  (with x-can-win-smart [[:o :x  nil]
+                         [:x nil nil]
+                         [:o nil nil]])
+
   (it "recognizes imminent victories"
     (should= [0 2] (next-move @smart-ai-player @x-can-win-row))
     (should= [2 0] (next-move @smart-ai-player @x-can-win-col)))
 
-  )
+  (it "recognizes distant victories"
+    (should= [1 1] (next-move @smart-ai-player @x-can-win-row))
+    )
+
+  (context "scoring the board"
+    (it "scores based on current victor"
+      (should= 1 (score-board @x-won-row :x :x))
+      (should= -1 (score-board @x-won-row :x :o))
+      (should= 1 (score-board @x-won-col :x :x))
+      (should= -1 (score-board @x-won-col :x :o)))
+
+    (it "scores based on immediate victor"
+      (should= 1 (score-board @x-can-win-row :x :x))
+      (should= -1 (score-board @x-can-win-row :x :o))
+      (should= 1 (score-board @x-can-win-col :x :x))
+      (should= -1 (score-board @x-can-win-col :x :o))
+    )
+
+  ))
--- src/tictactoe/smart_ai_player.clj
+++ src/tictactoe/smart_ai_player.clj
@@ -1,6 +1,6 @@
 (ns tictactoe.smart-ai-player
   (:use [tictactoe.move-source]
-        [tictactoe.board-utils :only [empty-squares game-winner-after-move update-board]]))
+        [tictactoe.board-utils :only [empty-squares game-winner game-winner-after-move update-board]]))
 
 (defn winning-next-moves [board signature]
   (let [next-moves (empty-squares board)]
@@ -8,6 +8,43 @@
 
 (defn winning-move [board signature]
   (first (winning-next-moves board signature)))
+
+(defn get-score-from-winner [intended-winner winner]
+  (if winner
+    (if (= intended-winner winner)
+      1
+      -1)
+    nil))
+
+(defn other-player [player]
+  (case
+    player
+    :x
+    :o
+    :o
+    :x))
+
+(defn score-board [board player intended-winner]
+  (let [winner (game-winner board)
+        potential-future-boards (map #(update-board board % player) (empty-squares board))]
+
+    (cond
+      winner
+      (get-score-from-winner intended-winner winner)
+      (empty? potential-future-boards)
+      0
+      :else
+     (let [other-player (other-player player)
+           best-next-board-for-player (if
+                                        (= player intended-winner)
+                                        (apply max-key #(score-board % other-player intended-winner) potential-future-boards)
+                                        (apply min-key #(score-board % other-player intended-winner) potential-future-boards))]
+       (score-board best-next-board-for-player other-player intended-winner)
+
+
+
+      ))))
+
 
 (deftype SmartAiPlayer [signature]
   MoveSource
