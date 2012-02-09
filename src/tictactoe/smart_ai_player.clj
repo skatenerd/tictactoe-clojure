e86b510589e8295c85a00ee1fbea7bcd24706955
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-02-09 10:58:06 -0600

    refactor a little

--- src/tictactoe/smart_ai_player.clj
+++ src/tictactoe/smart_ai_player.clj
@@ -2,6 +2,8 @@
   (:use [tictactoe.move-source]
         [tictactoe.board-utils :only [empty-squares potential-next-boards game-winner game-winner-after-move update-board]]))
 
+(declare score-board)
+
 (defn winning-next-moves [board signature]
   (let [next-moves (empty-squares board)]
     (filter #(= signature (game-winner-after-move board % signature)) next-moves)))
@@ -24,37 +26,39 @@
     :o
     :x))
 
+
+(defn best-next-board-for-player [next-boards player intended-winner]
+  (let [goal-fn (if (= player intended-winner)
+                  max-key
+                  min-key)]
+
+    (apply
+      goal-fn
+      #(score-board
+         %
+         (other-player player)
+         intended-winner)
+      next-boards)))
+
+
 (defn score-board [board player intended-winner]
   (let [winner
         (game-winner board)
-        potential-future-boards
+        next-boards
         (potential-next-boards board player)]
     (cond
       winner
       (get-score-from-winner intended-winner winner)
-      (empty? potential-future-boards)
+      (empty? next-boards)
       0
       :else
-      (let [other-player (other-player player)
-            best-next-board-for-player (if
-                                         (= player intended-winner)
-                                         (apply
-                                           max-key
-                                           #(score-board
-                                              %
-                                              other-player
-                                              intended-winner)
-                                           potential-future-boards)
-                                         (apply
-                                           min-key
-                                           #(score-board
-                                              %
-                                              other-player
-                                              intended-winner)
-                                           potential-future-boards))]
+      (let [best-next-board (best-next-board-for-player
+                              next-boards
+                              player
+                              intended-winner)]
         (score-board
-          best-next-board-for-player
-          other-player
+          best-next-board
+          (other-player player)
           intended-winner)))))
 
 (defn score-move [board move player]
