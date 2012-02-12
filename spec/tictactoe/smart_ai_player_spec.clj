1c8d564a05a5d91de23ecb9bcc3264c3eb7e169f
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-02-12 12:29:55 -0600

    refactor to use game-state record.  need to figure out why test is failing

--- spec/tictactoe/smart_ai_player_spec.clj
+++ spec/tictactoe/smart_ai_player_spec.clj
@@ -2,7 +2,8 @@
   (:use [speclj.core]
         [tictactoe.smart-ai-player]
         [tictactoe.move-source]
-        [tictactoe.board-utils :only [empty-board]]))
+        [tictactoe.board-utils :only [empty-board]]
+        [tictactoe.game-state]))
 
 (describe "scoring boards and generating moves: "
 
@@ -59,47 +60,63 @@
 
   (context "scoring the board"
     (context "from perspective of player x"
-      (binding [is-intended-winner (fn [signature] (= signature :x))]
-        (it "scores based on current victor"
-          ;(should= -1 (first (evaluate-board @x-won-row :x :o {})))
-          (should= 1 (first (evaluate-board @x-won-col :x {})))
-          (should= -1 (first (evaluate-board @x-won-col :x {}))))
+      (it "scores based on current victor"
+        (binding [is-intended-winner (fn [signature] (= signature :x))]
+          (should= 1 (first (evaluate-board (new-game-state @x-won-col :x) {})))))
 
-        (it "scores based on immediate victor"
-          (should= 1 (first (evaluate-board @x-can-win-row :x {})))
-          ;(should= -1 (first (evaluate-board @x-can-win-row :x :o {})))
-          (should= 1 (first (evaluate-board @x-can-win-col :x {})))
-          ;(should= -1 (first (evaluate-board @x-can-win-col :x :o {})))
-          )
-        (it "recognizes guaranteed-ties"
-          (should= 0 (first (evaluate-board @guaranteed-tie :o {}))))
+      (it "scores based on immediate victor"
+        (binding [is-intended-winner (fn [signature] (= signature :x))]
+          (should= 1 (first (evaluate-board (new-game-state @x-can-win-row :x) {})))
+          (should= 1 (first (evaluate-board (new-game-state @x-can-win-col :x) {})))))
 
-        (it "scores based on distant victor"
-          (should= 1 (first (evaluate-board @x-can-win-smart :x {}))))
 
-        (it "recognizes current ties"
-          (should= 0 (first (evaluate-board @current-tie :x {}))))))
+      (it "recognizes guaranteed-ties"
+        (binding [is-intended-winner (fn [signature] (= signature :x))]
+          (prn (evaluate-board (new-game-state @guaranteed-tie :o) {}))
+          (should= 0 (first (evaluate-board (new-game-state @guaranteed-tie :o) {})))))
+
+      (it "recognizes current ties"
+        (binding [is-intended-winner (fn [signature] (= signature :x))]
+          (should= 0 (first (evaluate-board (new-game-state @current-tie :x) {})))))
+
+      (it "scores based on distant victor"
+        (binding [is-intended-winner (fn [signature] (= signature :x))]
+          (should= 1 (first (evaluate-board (new-game-state @x-can-win-smart :x) {}))))))
+
 
       (context "from the perspective of player o"
         (it "recognizes potential for opponent to throw game"
           (binding [is-intended-winner (fn [signature] (= signature :o))]
-            (should= (/ 1 2) (first (evaluate-board @guaranteed-tie :o {})))))))
+            (should= (/ 1 2) (first (evaluate-board
+                                      (new-game-state
+                                        @guaranteed-tie
+                                        :o)
+                                      {})))))
+        ;(should= -1 (first (evaluate-board @x-can-win-row :x :o {})))
+        ;(should= -1 (first (evaluate-board @x-can-win-col :x :o {})))
+        ;(should= -1 (first (evaluate-board @x-won-row :x :o {})))
+
+        ))
 
   (context "caching board scores"
     (context "from perspective of player x"
-      (binding [is-intended-winner (fn [signature] (= signature :x))]
-
         (it "caches a win when it encounters one"
-          (let [[score new-cached-situations] (evaluate-board @x-won-row :x {})]
-            (should= {[@x-won-row :x] 1} new-cached-situations)))
+          (binding [is-intended-winner (fn [signature] (= signature :x))]
+            (let [[score new-cached-situations] (evaluate-board (new-game-state @x-won-row :x) {})]
+              (should= {(new-game-state @x-won-row :x) 1} new-cached-situations))))
 
-        (it "recognizes guaranteed-ties, with potential for throwing game"
-          (let [[score learned-info] (evaluate-board @guaranteed-tie :o {})]
-            (should= {[@guaranteed-tie :o] 0} learned-info)))
+        (it "recognizes guaranteed-ties"
+          (binding [is-intended-winner (fn [signature] (= signature :x))]
+            (let [[score learned-info] (evaluate-board (new-game-state @guaranteed-tie :o) {})]
+              (should= {(new-game-state @guaranteed-tie :o) 0} learned-info))))
 
         (it "recognizes current ties"
-          (let [[score learned-info] (evaluate-board @current-tie :x {})]
-            (should= {[@current-tie :x] 0 [@current-tie :o] 0} learned-info))))))
+          (binding [is-intended-winner (fn [signature] (= signature :x))]
+            (let [[score learned-info] (evaluate-board (new-game-state @current-tie :x) {})]
+              (should=
+                {(new-game-state @current-tie :x) 0
+                 (new-game-state @current-tie :o) 0}
+                learned-info))))))
 
 
   (context "evaluating a move"
@@ -107,12 +124,13 @@
 
       (it "scores a winning move"
         (binding [is-intended-winner (fn [signature] (= signature :x))]
-          (should= 1 (first (evaluate-move @x-can-win-row [0 2] :x {})))))
+          (should= 1 (first (evaluate-move (new-game-state @x-can-win-row :x) [0 2] {})))))
 
       (it "scores a distantly winning move"
         (binding [is-intended-winner (fn [signature] (= signature :x))]
-          (should= 1 (first (evaluate-move @x-can-win-smart [1 1] :x {})))
-          (should= -1 (first (evaluate-move @x-can-win-smart [0 2] :x {})))))))
+          (let [game-state (new-game-state @x-can-win-smart :x)]
+          (should= 1 (first (evaluate-move game-state [1 1] {})))
+          (should= -1 (first (evaluate-move game-state [0 2] {}))))))))
 
 ;  (context "scoring a collection of moves"
 ;    (it "evaluates scores on an empty collection of boards"
@@ -145,8 +163,8 @@
 
 (describe "utilities"
   (it "adds to cache correctly"
-    (should= {[:x :y] 24} (add-to-cache {} :x :y 24))
-    )
+    (let [game-state (new-game-state :foo :bar)]
+    (should= {game-state 24} (add-to-cache {} game-state 24))))
 
   (it "computes other player correctly"
     (should= :x (other-player :o))
--- src/tictactoe/smart_ai_player.clj
+++ src/tictactoe/smart_ai_player.clj
@@ -4,7 +4,10 @@
                                       potential-next-boards
                                       game-winner
                                       game-winner-after-move
-                                      update-board]]))
+                                      update-board]]
+        [tictactoe.game-state]))
+
+(def is-intended-winner)
 
 (declare evaluate-board score-move compute-next-move-as-player evaluate-move other-player add-to-cache)
 
@@ -12,21 +15,16 @@
 (deftype SmartAiPlayer [signature]
   MoveSource
   (next-move [this board]
-    (compute-next-move-as-player signature board))
-  )
+    (compute-next-move-as-player (new-game-state board signature))))
 
 (defn new-smart-ai-player [signature]
   (SmartAiPlayer. signature))
 
 
-
-(def is-intended-winner)
-
-
-(defn compute-next-move-as-player [signature board]
-  (binding [is-intended-winner #(= % signature)]
-    (let [possible-moves (empty-squares board)
-          score-move-with-fixed-board #(score-move board % signature {})
+(defn compute-next-move-as-player [game-state]
+  (binding [is-intended-winner #(= % (:player game-state))]
+    (let [possible-moves (empty-squares (:board game-state))
+          score-move-with-fixed-board #(score-move game-state % {})
           optimal-move (apply
                           max-key
                           score-move-with-fixed-board
@@ -34,16 +32,13 @@
       optimal-move)))
 
 
-(defn score-move [board move player cache]
-  (let [[score cached-situations] (evaluate-move board move player cache)]
+(defn score-move [game-state move cache]
+  (let [[score cached-situations] (evaluate-move game-state move cache)]
     score))
 
-(defn winning-next-moves [board signature]
-  (let [next-moves (empty-squares board)]
-    (filter #(= signature (game-winner-after-move board % signature)) next-moves)))
-
-(defn winning-move [board signature]
-  (first (winning-next-moves board signature)))
+(defn winning-next-moves [game-state]
+  (let [next-moves (empty-squares (:board game-state))]
+    (filter #(= (:player game-state) (game-winner-after-move game-state %)) next-moves)))
 
 (defn compute-final-score [winner]
   (if winner
@@ -58,32 +53,32 @@
     (/ (count (filter #(= % 1) scores-of-future-boards))
       (count scores-of-future-boards))))
 
-(defn minimize-score-as-human [player board cached-scores]
+(defn minimize-score-as-human [game-state cached-scores]
   (let
-    [possible-moves (empty-squares board)
+    [possible-moves (empty-squares (:board game-state))
       scores-and-caches-of-future-boards
      (map
-       #(evaluate-move board % player cached-scores)
+       #(evaluate-move game-state % cached-scores)
        possible-moves)
      scores-of-future-boards (map first scores-and-caches-of-future-boards)
      caches-of-future-boards (map second scores-and-caches-of-future-boards)]
     [(compute-score-as-human scores-of-future-boards) {}]))
 
-(defn maximize-score-rationally [player board cached-scores]
+(defn maximize-score-rationally [game-state cached-scores]
   (let
-    [possible-moves (empty-squares board)
+    [possible-moves (empty-squares (:board game-state))
       scores-and-caches-of-future-boards
      (map
-       #(evaluate-move board % player cached-scores)
+       #(evaluate-move game-state % cached-scores)
        possible-moves)
      scores-of-future-boards (map first scores-and-caches-of-future-boards)
      caches-of-future-boards (map second scores-and-caches-of-future-boards)]
     [(apply max scores-of-future-boards) {}]))
 
-(defn score-by-thinking-ahead [player board cached-scores]
-  (if (is-intended-winner player)
-    (maximize-score-rationally player board cached-scores)
-    (minimize-score-as-human player board cached-scores)))
+(defn score-by-thinking-ahead [game-state cached-scores]
+  (if (is-intended-winner (:player game-state))
+    (maximize-score-rationally game-state cached-scores)
+    (minimize-score-as-human game-state cached-scores)))
 
 ;(defn- evaluate-board-and-update-caches [player intended-winner {:keys [total-cache learned-situations scores]} board]
 ;  (let [[score new-situations-to-record] (evaluate-board board player total-cache)]
@@ -102,29 +97,30 @@
 ;     :scores []}
 ;    boards))
 
-
-(defn evaluate-board [board player cached-scores]
+(defn evaluate-board [game-state cached-scores]
   (let [looked-up-winner
-        (cached-scores [board player])
+        (cached-scores [game-state])
         computed-winner
-        (game-winner board)]
+        (game-winner (:board game-state))]
     (cond
       looked-up-winner
       [looked-up-winner {}]
       computed-winner
       (let [score (compute-final-score computed-winner)]
-        [score (add-to-cache {} board player score)])
-      (empty? (empty-squares board))
+        [score (add-to-cache {} game-state score)])
+      (empty? (empty-squares (:board game-state)))
       [0 (->
         {}
-        (add-to-cache board player 0)
-        (add-to-cache board (other-player player) 0))]
+        (add-to-cache game-state 0)
+        (add-to-cache (new-game-state (:board game-state) (other-player (:player game-state))) 0))]
       :else
-      (score-by-thinking-ahead player board cached-scores))))
+      (score-by-thinking-ahead game-state cached-scores))))
 
-(defn evaluate-move [board move player cache]
-  (let [resulting-board (update-board board move player)]
-    (evaluate-board resulting-board (other-player player) cache)))
+(defn evaluate-move [game-state move cache]
+  (let [resulting-board (update-board (:board game-state) move (:player game-state))
+        other-player (other-player (:player game-state))
+        resulting-game-state (new-game-state resulting-board other-player)]
+    (evaluate-board resulting-game-state cache)))
 
 (defn other-player [player]
   (case
@@ -134,5 +130,5 @@
     :o
     :x))
 
-(defn add-to-cache [cache board player score]
-  (conj cache {[board player] score}))
+(defn add-to-cache [cache game-state score]
+  (conj cache {game-state score}))
